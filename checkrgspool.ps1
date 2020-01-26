03a6bb0fd42079306d80af40d8657190ffe8be08
Konrad Sagala <ksagala@outlook.com>
2020-01-26 17:55:17 +0100

    1st commit

---
+++ checkrgspool.ps1
@@ -0,0 +1,30 @@
+function checkRGSpool {
+    param (
+        [string]$poolname
+    )
+
+    $agentgroups = Get-CsRgsAgentGroup | Where-Object OwnerPool -eq $poolname
+
+    ForEach ($agentgr in $agentgroups) {
+        $groupname = $agentgr.Name
+        write-host "Processing $groupname"
+        if ($agentgr.AgentsByUri.count -gt 0) {
+            $agents = $agentgr.AgentsByUri
+            foreach ($agent in $agents) {
+                $agent = $Agent.tostring()
+                # write-host "Agent $agent"
+
+                Try {
+                    $currentagent = get-csuser $agent -ErrorAction Stop
+                }
+                Catch [Exception] {
+                    write-host "Ghost agent found: $agent" -ForegroundColor Yellow
+                }
+                $agentlist += $agent
+            }
+        }
+        else {
+            write-host "$groupname has no Agents" -ForegroundColor Yellow
+        }
+    }
+}
